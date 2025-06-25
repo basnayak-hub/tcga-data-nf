e524f6b3a1be6e725ba74b1471c07c4ce5322ccc
violafanfani <viola.fanfani@gmail.com>
2025-06-25 20:08:09 +0000

    fixing capital r

--- assets/make_manifest.r
+++ assets/make_manifest.r
@@ -3,11 +3,12 @@
 # HG38 genome build with gencode v36.
 # This is from the public manifest resources here: https://zwdzwd.github.io/InfiniumAnnotation
 
-library(NetSciDataCompanion)
-my_friend = NetSciDataCompanion::CreateNetSciDataCompanionObject()
+library(NetworkDataCompanion)
+my_friend = NetworkDataCompanion::CreateNetworkDataCompanionObject()
 
 my_manifest = my_friend$mapProbesToGenes(probelist = NULL, # this is the default, and maps every probe in the manifest
                                          rangeUp = 200,
-                                         rangeDown = 0)
+                                         rangeDown = 0,
+                                         localManifestPath="/home/ubuntu/tcga-ov-subtype/data/config/HM27.hg38.manifest.tsv")
 
-write.csv(my_manifest,file="450k_manifest_TSS200_TSS0_one_probe_to_many_genes.csv")
+write.csv(my_manifest,file="27k_manifest_TSS200_TSS0_one_probe_to_many_genes.csv")
